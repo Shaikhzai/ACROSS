d5df85e5c1a47cbedb2e0e82e39cda3a30cb0325
Shaikhzai <shaikhston1123@gmail.com>
2024-08-28 12:03:14 +0530

    change

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "The Across Group",
-  description: "Created by Zaid & Team",
+  description: "Website created by Zaid & Team",
 };
 
 export default function RootLayout({ children }) {
